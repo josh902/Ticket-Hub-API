cd73e9a36d9a27a5ff023dedf0d22526040a0b1f
Josh Runas <W0468743@nscc.ca>
2025-03-25 11:32:14 -0300

    update test

--- Ticket Hub API/Controllers/PurchaseController.cs
+++ Ticket Hub API/Controllers/PurchaseController.cs
@@ -20,7 +20,7 @@ namespace Ticket_Hub_API.Controllers
 
             // Set up Azure Queue
             string queueName = "tickethub";
-            string connectionString = "DefaultEndpointsProtocol=https;AccountName=nscc0468743storageacct;AccountKey=07DplxGAypPorxXJHXwflTAaXcdaB5Z2XJpeQ9uQBr/p9BMMlKbxdNcDpY25yQ1vsMnvch6MI2AS+AStIkYJiQ==;EndpointSuffix=core.windows.net"; // Replace this
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=nscc0468743storageacct;AccountKey=07DplxGAypPorxXJHXwflTAaXcdaB5Z2XJpeQ9uQBr/p9BMMlKbxdNcDpY25yQ1vsMnvch6MI2AS+AStIkYJiQ==;EndpointSuffix=core.windows.net"; // Replace this make sure to i ahe the azure thing
 
             var queueClient = new QueueClient(connectionString, queueName);
             await queueClient.CreateIfNotExistsAsync();
